698f393cc33e0c7038b7c878e500f65fcd415583
Arun Singh <arun.singh2@assaabloy.com>
2019-02-19 16:37:10 +0530

    zull filter

--- api-gateway/src/main/java/com/joker/apigateway/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/joker/apigateway/apigateway/ApiGatewayApplication.java
@@ -3,15 +3,23 @@ package com.joker.apigateway.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 
 }
